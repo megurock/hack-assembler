a560b197bdfc74582240fe570b4489cd1d729304
Eiji Meguro <eiji@fork.co.jp>
2022-07-30 21:58:53 +0900

    Add Parser

---
+++ __tests__/Parser.test.ts
@@ -0,0 +1,60 @@
+import Parser from '../src/script/Parser'
+
+const singleLineCommand = '@1'
+const doubleLoneCommand = `@1
+@2`
+
+/**
+ * The following test code also verifies correct operation, even if they include some whitespace.
+ */
+describe('Parser', () => {
+  //
+  it('should identify if it has next command.', () => {
+    expect(new Parser(singleLineCommand).hasMoreCommands).toBeFalsy()
+    expect(new Parser(doubleLoneCommand).hasMoreCommands).toBeTruthy()
+  })
+  //
+  it('should proceed to the next command line.', () => {
+    const parser = new Parser(doubleLoneCommand)
+    parser.advance()
+    expect(parser.command).toBe('@2')
+  })
+  //
+  it('should return the command type.', () => {
+    expect(new Parser(' @1').commandType).toBe(Parser.A_COMMAND)
+    expect(new Parser(' M=1 ').commandType).toBe(Parser.C_COMMAND)
+    expect(new Parser(' (LOOP) ').commandType).toBe(Parser.L_COMMAND)
+  })
+  //
+  it('should return the command symbol.', () => {
+    expect(new Parser(' @1 ').symbol).toBe('1')
+    expect(new Parser(' M=1 ').symbol).toBeNull()
+    expect(new Parser(' (LOOP) ').symbol).toBe('LOOP')
+  })
+  //
+  it('should return the C instruction dest mnemonic.', () => {
+    expect(new Parser(' D=M ').dest).toEqual('D')
+    expect(new Parser(' (0;JPM) ').dest).toBeNull()
+    expect(new Parser(' @1 ').dest).toBeNull()
+  })
+  //
+  it('should return the C instruction comp mnemonic.', () => {
+    expect(new Parser(' D=M ').comp).toEqual('M')
+    expect(new Parser(' D=1;JPM ').comp).toEqual('1')
+    expect(new Parser(' 0;JMP ').comp).toEqual('0')
+    expect(new Parser(' @1 ').comp).toBeNull()
+  })
+  //
+  it('should return the C instruction jump mnemonic.', () => {
+    expect(new Parser(' D=1;JGT ').jump).toEqual('JGT')
+    expect(new Parser(' 0;JMP ').jump).toEqual('JMP')
+    expect(new Parser(' D=M ').jump).toBeNull()
+    expect(new Parser(' @1 ').dest).toBeNull()
+  })
+  //
+  it('should update the assembly.', () => {
+    const parser = new Parser('@1')
+    parser.setAssembly('D=1')
+    expect(parser.commandType === Parser.C_COMMAND).toBeTruthy()
+  })
+})
---
+++ src/script/Parser.ts
@@ -0,0 +1,116 @@
+export default class Parser {
+  public static readonly A_COMMAND = 'A_COMMAND'
+  public static readonly C_COMMAND = 'C_COMMAND'
+  public static readonly L_COMMAND = 'L_COMMAND'
+  protected lines: string[] = []
+  protected head = 0
+
+  /**
+   *
+   * @param assembly
+   */
+  public constructor(assembly: string) {
+    this.setAssembly(assembly)
+  }
+
+  /**
+   *
+   * @param value
+   */
+  public setAssembly(value: string) {
+    const breakCode = '\n'
+    this.lines = value.replace(/\r?\n/g, breakCode).split(breakCode)
+    this.head = 0
+  }
+
+  /**
+   *
+   */
+  public get command() {
+    return this.lines[this.head].trim()
+  }
+
+  /**
+   *
+   */
+  public advance() {
+    this.head++
+  }
+
+  /**
+   *
+   */
+  public get hasMoreCommands() {
+    return this.head < this.lines.length - 1
+  }
+
+  /**
+   *
+   */
+  public get commandType() {
+    if (this.command.startsWith('@')) return Parser.A_COMMAND
+    else if (/^\(.+\)$/.test(this.command)) return Parser.L_COMMAND
+    else return Parser.C_COMMAND
+  }
+
+  /**
+   *
+   */
+  public get symbol() {
+    switch (this.commandType) {
+      case Parser.A_COMMAND:
+        return this.command.slice(1)
+      case Parser.L_COMMAND:
+        return this.command.replace(/\((.+)\)/, '$1')
+      default:
+        return null
+    }
+  }
+
+  /**
+   *
+   */
+  public get dest() {
+    if (this.commandType === Parser.C_COMMAND) {
+      if (this.hasDest()) return this.command.split('=')[0]
+    }
+    return null
+  }
+
+  /**
+   *
+   */
+  public get jump() {
+    if (this.commandType === Parser.C_COMMAND) {
+      if (this.hasJump()) return this.command.split(';')[1]
+    }
+    return null
+  }
+
+  /**
+   *
+   */
+  public get comp() {
+    if (this.commandType === Parser.C_COMMAND) {
+      let comp = this.command
+      if (this.hasDest()) comp = comp.split('=')[1]
+      if (this.hasJump()) comp = comp.split(';')[0]
+      return comp
+    }
+    return null
+  }
+
+  /**
+   *
+   */
+  protected hasDest() {
+    return /=/.test(this.command)
+  }
+
+  /**
+   *
+   */
+  protected hasJump() {
+    return /;/.test(this.command)
+  }
+}
